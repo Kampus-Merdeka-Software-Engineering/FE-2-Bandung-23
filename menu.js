82eabd877160c6d8fbbd5cacc2884d5948aaf8e2
FadhilAlJufri <afkaolshop23@gmail.com>
2023-11-13 18:24:24 +0700

    ...

--- menu.js
+++ menu.js
@@ -15,10 +15,50 @@ links.forEach((link) => {
   }
 });
 
+const listItems = document.querySelectorAll(".list-kat li");
+
+// Menambahkan event listener pada setiap elemen <li>
+listItems.forEach(function (item, index) {
+  item.addEventListener("click", function () {
+    // Menghapus class "active" dari semua elemen <li>
+    listItems.forEach(function (li) {
+      li.classList.remove("active");
+    });
+
+    // Menambah class "active" pada elemen yang diklik
+    item.classList.add("active");
+  });
+});
+
 const hamburgerButtonElement = document.querySelector("#hamburger");
 const drawerElement = document.querySelector(".nav-list");
 
 hamburgerButtonElement.addEventListener("click", () => {
   hamburgerButtonElement.classList.toggle("active");
   drawerElement.classList.toggle("active");
+});
+
+document.querySelectorAll(".jumlah-order").forEach(function (element) {
+  var minusButton = element.querySelector(".minus");
+  var plusButton = element.querySelector(".plus");
+  var jumlahElement = element.querySelector(".num");
+  var jumlah = parseInt(jumlahElement.innerText);
+
+  minusButton.addEventListener("click", function () {
+    if (jumlah > 0) {
+      jumlah--;
+      updateNilai();
+    }
+  });
+
+  plusButton.addEventListener("click", function () {
+    if (jumlah < 9) {
+      jumlah++;
+      updateNilai();
+    }
+  });
+
+  function updateNilai() {
+    jumlahElement.innerText = jumlah;
+  }
 });
